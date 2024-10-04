65b5a4265419f0aa47ff64ceff5a77443cd90b8a
Niklas Alexander Shern <nashern@protonmail.com>
2024-10-04 14:43:04 +0200

    fix: remove unused code

--- converter.py
+++ converter.py
@@ -23,12 +23,3 @@ class Converter:
             return (
                 self.prices[from_currency_name] / self.prices[to_currency_name]
             ) * amount
-
-
-if __name__ == "__main__":
-    c = Converter()
-    c.set_price_per_unit("eth", 100)
-    c.set_price_per_unit("btc", 2)
-
-    print(c.convert(from_currency_name="eth", to_currency_name="btc", amount=5))
-    print(c.convert(from_currency_name="btc", to_currency_name="eth", amount=65))
